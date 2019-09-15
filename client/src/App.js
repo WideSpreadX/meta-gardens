46f622c95386edf7c5d4153de3e4e3aef262b35f
WideSpreadX <48499429+WideSpreadX@users.noreply.github.com>
2019-09-15 16:33:37 -0400

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Meta Garden Services</h1>
+    </div>
+  );
+}
+
+export default App;
